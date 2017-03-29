abaa2562d3f87e0b70238f52a5fac5b8383271e8
nise <nils.semmelrock@gmail.com>
2017-03-29 09:46:51 +0200

    Add project files.

---
+++ GraphDrawer/DrawGraphFrom.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GraphDrawer
+{
+    public partial class DrawGraphForm : Form
+    {
+        private Graph graph;
+        private Graphics graphics;
+        private Image doubleBufferImage;
+        private Graphics doubleBufferGraphics;
+
+        // ui features
+        private Vertex movingVertex = null;
+        private Vertex drawingEdgeVertex = null;
+        private int mouseMoveX = 0;
+        private int mouseMoveY = 0;
+
+        // drawing materials
+        private Pen blackPen = new Pen(Color.Black);
+        private Brush fillVertexWhiteBrush = new SolidBrush(Color.White);
+        private Brush fillVertexRedBrush = new SolidBrush(Color.Red);
+        private Brush nameBrush = new SolidBrush(Color.Gray);
+        private Font nameFont = new Font("Arial", 12);
+
+        public DrawGraphForm()
+        {
+            // build gui
+            InitializeComponent();
+
+            // init empty graph
+            graph = new Graph();
+
+            // init graphics for drawing
+            graphics = drawingArea.CreateGraphics();
+            doubleBufferImage = new Bitmap(drawingArea.Width, drawingArea.Height);
+            doubleBufferGraphics = Graphics.FromImage(doubleBufferImage);
+
+            // init event handler
+            this.KeyPreview = true;
+            this.KeyPress += new KeyPressEventHandler(DrawGraphForm_KeyPress);
+            this.Resize += new EventHandler(DrawGraphForm_Resize);
+            drawingArea.MouseDown += new MouseEventHandler(drawingArea_MouseDown);
+            drawingArea.MouseMove += new MouseEventHandler(drawingArea_MouseMove);
+            drawingArea.MouseUp += new MouseEventHandler(drawingArea_MouseUp);
+            drawingArea.MouseDoubleClick += new MouseEventHandler(drawingArea_MouseDoubleClick);
+        }
+
+        private void drawingArea_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // add vertex or draw edge from vertex A to vertex B
+            if (e.Button == MouseButtons.Left)
+            {
+                Vertex v = getVertexForCoordOrNull(e);
+                if (v != null)
+                {
+                    if (drawingEdgeVertex == null)
+                    {
+                        mouseMoveX = e.X;
+                        mouseMoveY = e.Y;
+                        drawingEdgeVertex = v;
+                    }
+                    else
+                    {
+                        graph.addEdge(new Edge(drawingEdgeVertex, v));
+                        drawingEdgeVertex = null;
+                    }
+                }
+                else
+                {
+                    graph.addVertex(new Vertex(1.0f * e.X / drawingArea.Width, 1.0f * e.Y / drawingArea.Height, ""));
+                }
+                drawGraph();
+            }
+        }
+
+        private Vertex getVertexForCoordOrNull(MouseEventArgs e)
+        {
+            float projX = 1.0f * e.X / drawingArea.Width;
+            float projY = 1.0f * e.Y / drawingArea.Height;
+            float minWH = Math.Min(drawingArea.Width, drawingArea.Height);
+            Vertex v = graph.getVertexByCoord(projX, projY, minWH / drawingArea.Width, minWH / drawingArea.Height);
+            return v;
+        }
+
+        private void drawingArea_MouseUp(object sender, MouseEventArgs e)
+        {
+            // stop moving vertex
+            movingVertex = null;
+        }
+
+        private void drawingArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            // move vertex
+            if (movingVertex != null)
+            {
+                if (e.X > 0 && e.X < drawingArea.Width)
+                {
+                    movingVertex.x = 1.0f * e.X / drawingArea.Width;
+                }
+                if (e.Y > 0 && e.Y < drawingArea.Height)
+                {
+                    movingVertex.y = 1.0f * e.Y / drawingArea.Height;
+                }
+                drawGraph();
+            }
+
+            // show help line for edge drawing
+            if (drawingEdgeVertex != null)
+            {
+                mouseMoveX = e.X;
+                mouseMoveY = e.Y;
+                drawGraph();
+            }
+        }
+
+        private void drawingArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            // left: select vertex for moving
+            if (e.Button == MouseButtons.Left)
+            {
+                Vertex v = getVertexForCoordOrNull(e);
+                if (v != null)
+                {
+                    movingVertex = v;
+                }
+                drawGraph();
+            }
+            // right: stop drawing edge or remove vertex
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (drawingEdgeVertex != null)
+                {
+                    drawingEdgeVertex = null;
+                }
+                else
+                {
+                    Vertex v = getVertexForCoordOrNull(e);
+                    if (v != null)
+                    {
+                        graph.remove(v);
+                    }
+                }
+                drawGraph();
+            }
+        }
+
+        private void DrawGraphForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case 's':
+                    graph = Graph.generateSampleGraph();
+                    drawGraph();
+                    break;
+                case 'c':
+                    graph = new Graph();
+                    drawGraph();
+                    break;
+                case 'b':
+                    if (drawingEdgeVertex != null)
+                    {
+                        graph.clearNamesOfVertices();
+                        graph.bfs(drawingEdgeVertex);
+                        drawingEdgeVertex = null;
+                        drawGraph();
+                    }
+                    break;
+                case 'd':
+                    if (drawingEdgeVertex != null)
+                    {
+                        graph.clearNamesOfVertices();
+                        graph.dfs(drawingEdgeVertex);
+                        drawingEdgeVertex = null;
+                        drawGraph();
+                    }
+                    break;
+            }
+        }
+
+        private void DrawGraphForm_Resize(object sender, System.EventArgs e)
+        {
+            doubleBufferImage.Dispose();
+            doubleBufferGraphics.Dispose();
+            graphics = drawingArea.CreateGraphics();
+            doubleBufferImage = new Bitmap(drawingArea.Width, drawingArea.Height);
+            doubleBufferGraphics = Graphics.FromImage(doubleBufferImage);
+            drawGraph();
+        }
+
+        private void drawGraph()
+        {
+            // drawingArea not visible
+            if (drawingArea.Width <= 0 || drawingArea.Height <= 0)
+            {
+                return;
+            }
+
+            // initialize double buffer
+            doubleBufferGraphics.FillRectangle(new SolidBrush(Color.White), 0, 0, drawingArea.Width, drawingArea.Height);
+
+            // draw graph - note: draw edges before vertices or lines will overlap ellipses
+            drawEdges(doubleBufferGraphics);
+            drawVertices(doubleBufferGraphics);
+            drawHelpLineForEdge(doubleBufferGraphics);
+
+            // draw double buffer
+            graphics.DrawImage(doubleBufferImage, 0, 0);
+        }
+
+        private void drawHelpLineForEdge(Graphics doubleBufferGraphics)
+        {
+            if (drawingEdgeVertex != null)
+            {
+                float v1x = drawingEdgeVertex.x * drawingArea.Width;
+                float v1y = drawingEdgeVertex.y * drawingArea.Height;
+                doubleBufferGraphics.DrawLine(blackPen, v1x, v1y, mouseMoveX, mouseMoveY);
+            }
+        }
+
+        private void drawVertices(Graphics doubleBufferGraphics)
+        {
+            int radMod = Math.Min(drawingArea.Width, drawingArea.Height);
+            for (int i = 0; i < graph.numVertices(); i++)
+            {
+                Vertex v = graph.getVertex(i);
+                float vx = drawingArea.Width * v.x - v.radius * radMod;
+                float vy = drawingArea.Height * v.y - v.radius * radMod;
+                float dia = 2 * v.radius * radMod;
+                if (drawingEdgeVertex != null && drawingEdgeVertex.Equals(v))
+                {
+                    doubleBufferGraphics.FillEllipse(fillVertexRedBrush, vx, vy, dia, dia);
+                }
+                else
+                {
+                    doubleBufferGraphics.FillEllipse(fillVertexWhiteBrush, vx, vy, dia, dia);
+                }
+                doubleBufferGraphics.DrawEllipse(blackPen, vx, vy, dia, dia);
+                doubleBufferGraphics.DrawString(v.name, nameFont, nameBrush, vx + dia, vy - dia / 2);
+            }
+        }
+
+        private void drawEdges(Graphics doubleBufferGraphics)
+        {
+            for (int i = 0; i < graph.numEdges(); i++)
+            {
+                Edge e = graph.getEdge(i);
+                float v1x = e.vertex1.x * drawingArea.Width;
+                float v1y = e.vertex1.y * drawingArea.Height;
+                float v2x = e.vertex2.x * drawingArea.Width;
+                float v2y = e.vertex2.y * drawingArea.Height;
+                doubleBufferGraphics.DrawLine(blackPen, v1x, v1y, v2x, v2y);
+            }
+        }
+    }
+}
---
+++ GraphDrawer/Graph.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphDrawer
+{
+    class Graph
+    {
+        private List<Vertex> vertices = new List<Vertex>();
+        private List<Edge> edges = new List<Edge>();
+
+        public void dfs(Vertex start)
+        {
+            List<Vertex> reached = new List<Vertex>();
+            Stack<Vertex> todo = new Stack<Vertex>();
+
+            Console.WriteLine("Not implemented.");
+        }
+
+        public void bfs(Vertex start)
+        {
+            List<Vertex> reached = new List<Vertex>();
+            Queue<Vertex> todo = new Queue<Vertex>();
+
+            Console.WriteLine("Not implemented.");
+        }
+
+        private List<Vertex> getNeighbours(Vertex u)
+        {
+            List<Vertex> neighbours = new List<Vertex>();
+            foreach (Edge edge in edges)
+            {
+                if (u.Equals(edge.vertex1))
+                {
+                    neighbours.Add(edge.vertex2);
+                }
+                else if (u.Equals(edge.vertex2))
+                {
+                    neighbours.Add(edge.vertex1);
+                }
+            }
+            return neighbours;
+        }
+
+        public static Graph generateSampleGraph()
+        {
+            Graph g = new Graph();
+            for (int i = 1; i <= 11; i++)
+            {
+                for (int j = 1; j <= 11; j++)
+                {
+                    String name = "";// + ((i - 1) * 11 + j);
+                    Vertex v = new Vertex(i * 1.0f / 12, j * 1.0f / 12, name);
+                    g.addVertex(v);
+                }
+            }
+
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    if (j < 10)
+                    {
+                        g.addEdge(new Edge(g.getVertex(i * 11 + j), g.getVertex(i * 11 + j + 1)));
+                    }
+                    if (i < 10)
+                    {
+                        g.addEdge(new Edge(g.getVertex(i * 11 + j), g.getVertex((i + 1) * 11 + j)));
+                    }
+                }
+            }
+            return g;
+        }
+
+        public Vertex getVertexByCoord(float x, float y, float rx, float ry)
+        {
+            foreach (Vertex vertex in vertices)
+            {
+                if (vertex.isAtCoord(x, y, rx, ry))
+                {
+                    return vertex;
+                }
+            }
+            return null;
+        }
+
+        public void addVertex(Vertex v)
+        {
+            vertices.Add(v);
+        }
+
+        public Vertex getVertex(int i)
+        {
+            return vertices.ElementAt(i);
+        }
+
+        public int numVertices()
+        {
+            return vertices.Count;
+        }
+
+        public int numEdges()
+        {
+            return edges.Count;
+        }
+
+        public Edge getEdge(int i)
+        {
+            return edges.ElementAt(i);
+        }
+
+        public void addEdge(Edge e)
+        {
+            if (!e.vertex1.Equals(e.vertex2))
+            {
+                edges.Add(e);
+            }
+        }
+
+        internal void remove(Vertex v)
+        {
+            for (int i = edges.Count() - 1; i >= 0; i--)
+            {
+                Edge edge = edges.ElementAt(i);
+                if (v.Equals(edge.vertex1) || v.Equals(edge.vertex2))
+                {
+                    edges.Remove(edge);
+                }
+            }
+            vertices.Remove(v);
+        }
+
+        internal void clearNamesOfVertices()
+        {
+            foreach (Vertex vertex in vertices)
+            {
+                vertex.name = "";
+            }
+        }
+    }
+
+}
